2b89ec5caeb6d6c84f54ee8249273949530f4ac4
yzx <23371389@buaa.edu.cn>
2025-07-04 12:11:02 +0800

    修改了api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // const baseURL = 'http://big-event-vue-api-t.itheima.net'
-const baseURL = 'http://127.0.0.1:8000'
-// const baseURL = 'http://47.93.81.21:8000'
+// const baseURL = 'http://127.0.0.1:8000'
+const baseURL = 'http://47.93.81.21:8000'
 
 const instance = axios.create({
   // TODO 1. 基础地址，超时时间
